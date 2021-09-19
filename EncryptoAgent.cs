3808611a86df9f00414e33fb9df2646f89d3f856
BuildTools <unconfigured@null.spigotmc.org>
2021-09-19 11:56:27 +0200

    Add project files.

---
+++ EncryptoAgent.cs
@@ -0,0 +1,18 @@
+﻿
+using rlbot.flat;
+using RLBotDotNet;
+
+namespace EncryptoBot
+{
+	class EncryptoAgent : RLBotDotNet.Bot
+	{
+		public EncryptoAgent(string botName, int botTeam, int botIndex) : base(botName, botTeam, botIndex)
+		{
+			botName = "testName";
+		}
+		public override Controller GetOutput(GameTickPacket gameTickPacket)
+		{
+			return new Controller();
+		}
+	}
+}
---
+++ EncryptoBot GUI.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EncryptoBot
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class EncryptoBotGui : Window
+	{
+		Thread botManagerThread;
+		private ControlWriter consoleWriter = new ControlWriter();
+		const int port = 45031;
+		public EncryptoBotGui()
+		{
+			InitializeComponent();
+			RLBotDotNet.BotManager<EncryptoAgent> botManager = new RLBotDotNet.BotManager<EncryptoAgent>(0);
+			botManagerThread = new Thread(() => botManager.Start(port));
+			Console.SetOut(consoleWriter);
+		}
+	}
+	class ControlWriter : TextWriter
+	{
+		public Control control { get; private set;}
+		public override Encoding Encoding{get { return Encoding.ASCII; }}
+
+		public ControlWriter(Control _control)
+		{
+			control = _control;
+		}
+
+	}
+}
